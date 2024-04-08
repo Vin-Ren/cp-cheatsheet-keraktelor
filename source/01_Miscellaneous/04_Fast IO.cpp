bc4ad80cb41c7f07cb76a11716f785c7a07c871f
Richard Alison <richardalison86@gmail.com>
2024-04-08 16:00:12 +0900

    WF adjustments, part 2

--- source/01_Miscellaneous/04_Fast IO.cpp
+++ source/01_Miscellaneous/04_Fast IO.cpp
@@ -1,20 +1,33 @@
 int read() {
   char c;
-  do { c = getchar_unlocked(); } while(c < 33);
+  do {
+    c = getchar_unlocked();
+  } while(c < 33);
   int res = 0, mul = 1;
-  if(c == '-') { mul = -1; c = getchar_unlocked(); }
-  for(;'0' <= c && c <= '9'; c = getchar_unlocked()) {res = res * 10 + c - '0';}
+  if(c == '-') {
+    mul = -1;
+    c = getchar_unlocked();
+  }
+  for(; '0' <= c && c <= '9'; c = getchar_unlocked())
+    res = res * 10 + c - '0';
   return res * mul;
 }
 
 void write(int x) {
   static char wbuf[10];
-  if(x < 0) {putchar_unlocked('-'); x = -x;}
+  if(x < 0) {
+    putchar_unlocked('-');
+    x = -x;
+  }
   int idx = 0;
-  for(;x; x/=10) {wbuf[idx++] = x % 10;}
+  for(; x; x /= 10)
+    wbuf[idx++] = x % 10;
   if(idx == 0) putchar_unlocked('0');
   for(int i = idx - 1; i >= 0; --i) putchar_unlocked(wbuf[i] + '0');
 }
 void write(const char* s) {
-  while(*s) { putchar_unlocked(*s); ++s;}
+  while(*s) {
+    putchar_unlocked(*s);
+    ++s;
+  }
 }
