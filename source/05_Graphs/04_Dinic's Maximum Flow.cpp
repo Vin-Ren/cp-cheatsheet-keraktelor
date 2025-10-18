588254785301bb09d5d7e54d8f499a5aec9c8696
Vin-Ren <reaxumrx@gmail.com>
2025-10-18 20:28:53 +0700

    personalization and adjustments to graph templates

--- source/05_Graphs/04_Dinic's Maximum Flow.cpp
+++ source/05_Graphs/04_Dinic's Maximum Flow.cpp
@@ -2,80 +2,86 @@
 // O((V + E) sqrt(E)) if unit graph (turn scaling off)
 // O((V + E) sqrt(V)) if bipartite matching (turn scaling off)
 // indices are 0-based
-const ll INF = 1e18;
-
+template <typename T>
 struct Dinic {
   struct Edge {
-    int v;
-    ll cap, flow;
-    Edge(int _v, ll _cap): v(_v), cap(_cap), flow(0) {}
+    int v, rev;
+    T cap, flow;
   };
 
   int n;
-  ll lim;
-  vector<vector<int>> gr;
-  vector<Edge> e;
-  vector<int> idx, lv;
+  vector<vector<Edge>> g;
+  vector<int> lvl, it;
+  static constexpr T eps = (T)1e-9;
+  bool scaling;
 
-  bool has_path(int s, int t) {
-    queue<int> q;
-    q.push(s);
-    lv.assign(n, -1);
-    lv[s] = 0;
-    while(!q.empty()) {
-      int c = q.front();
-      q.pop();
-      if(c == t)
-        break;
-      for(auto& i : gr[c]) {
-        ll cur_flow = e[i].cap - e[i].flow;
-        if(lv[e[i].v] == -1 && cur_flow >= lim) {
-          lv[e[i].v] = lv[c] + 1;
-          q.push(e[i].v);
+  Dinic(int n, bool scaling = true): n(n), g(n), lvl(n), it(n), scaling(scaling) {}
+
+  void add_edge(int u, int v, T cap, bool directed = true) {
+    Edge a{v, (int)g[v].size(), cap, 0};
+    Edge b{u, (int)g[u].size(), directed ? 0 : cap, 0};
+    g[u].push_back(a);
+    g[v].push_back(b);
+  }
+
+  bool bfs(int s, int t, T lim) {
+    fill(lvl.begin(), lvl.end(), -1);
+    queue<int> q; q.push(s);
+    lvl[s] = 0;
+    while (!q.empty()) {
+      int u = q.front(); q.pop();
+      for (auto &e : g[u]) {
+        if (lvl[e.v] == -1 && e.cap - e.flow >= lim + eps) {
+          lvl[e.v] = lvl[u] + 1;
+          q.push(e.v);
         }
       }
     }
-    return lv[t] != -1;
+    return lvl[t] != -1;
   }
 
-  ll get_flow(int s, int t, ll left) {
-    if(!left || s == t)
-      return left;
-    while(idx[s] < (int) gr[s].size()) {
-      int i = gr[s][idx[s]];
-      if(lv[e[i].v] == lv[s] + 1) {
-        ll add = get_flow(e[i].v, t, min(left, e[i].cap - e[i].flow));
-        if(add) {
-          e[i].flow += add;
-          e[i ^ 1].flow -= add;
-          return add;
+  T dfs(int u, int t, T f) {
+    if (u == t || f <= eps) return f;
+    for (int &i = it[u]; i < (int)g[u].size(); i++) {
+      Edge &e = g[u][i];
+      if (lvl[e.v] == lvl[u] + 1 && e.cap - e.flow > eps) {
+        T ret = dfs(e.v, t, min(f, e.cap - e.flow));
+        if (ret > eps) {
+          e.flow += ret;
+          g[e.v][e.rev].flow -= ret;
+          return ret;
         }
       }
-      ++idx[s];
     }
     return 0;
   }
 
-  Dinic(int vertices, bool scaling = 1) : // toggle scaling here
-    n(vertices), lim(scaling ? 1 << 30 : 1), gr(n) {}
-
-  void add_edge(int from, int to, ll cap, bool directed = 1) {
-    gr[from].push_back(e.size());
-    e.emplace_back(to, cap);
-    gr[to].push_back(e.size());
-    e.emplace_back(from, directed ? 0 : cap);
+  T max_flow(int s, int t) {
+    T flow = 0;
+    T lim = scaling ? (T)(1 << 30) : 1;
+    while (lim > eps) {
+      while (bfs(s, t, lim)) {
+        fill(it.begin(), it.end(), 0);
+        while (true) {
+          T pushed = dfs(s, t, numeric_limits<T>::max());
+          if (pushed <= eps) break;
+          flow += pushed;
+        }
+      }
+      lim /= 2;
+    }
+    return flow;
   }
 
-  ll get_max_flow(int s, int t) { // call this
-    ll res = 0;
-    while(lim) {  // scaling
-      while(has_path(s, t)) {
-        idx.assign(n, 0);
-        while(ll add = get_flow(s, t, INF))
-          res += add;
-      }
-      lim >>= 1;
+  vector<int> min_cut(int s) {
+    vector<int> vis(n, 0);
+    queue<int> q; q.push(s); vis[s] = 1;
+    while (!q.empty()) {
+      int u = q.front(); q.pop();
+      for (auto &e : g[u])
+        if (!vis[e.v] && e.cap - e.flow > eps)
+          vis[e.v] = 1, q.push(e.v);
     }
-    return res;
+    return vis; // vis[i] == 1 -> S-side
   }
 };
